0bcfea61626c9f763b20829d3f684d4d37ea785a
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-09-18 04:47:48 +0200

    Adds `generateTree`

--- make-toc.sh
+++ make-toc.sh
@@ -3,6 +3,7 @@
 # This script generates a tiered Table of Contents from a
 # markdown document's header tags.
 
+
 # The number of CLI args passed
 ARGNUM=$#
 
@@ -17,7 +18,7 @@ SKIP=0
 if [[ $ARGNUM -gt 2  ]]; then
     case "$1" in
         -s|--skip)
-            SKIP=$(( $2 + 1  )) # the skip + offset
+            SKIP=$(( $2 + 1 )) # the skip + offset
             INPUT=$3
             OUTPUT=$4
             ;;
@@ -29,14 +30,20 @@ fi
 # list of bullet points relative to the top level.
 # $1 - A list of markdown headers
 function generateTree {
+    HEADERS=$1
+    # Log the file -> get first line -> isolate the header tag
+    TL=$( cat $HEADERS|head -1|sed -r "s/(#+) (.+)/\1/" ) 
+    SIZE=${#TL}
+    INDENT="-"
     
-    TL=$(grep -E "^\#+ [A-Za-z]+" $1|head -1)
-    echo $TL
-    # Turn headers into top-level bullet points
-    sed -i 's/^## /- /' $1
+    echo "Top level header: ${TL}"
 
-    # Turn subheaders into 2nd level bullet points
-    sed -i 's/^#### /    - /' $1
+    while [ $SIZE -lt 5  ]; do
+        sed -i "s/^$TL /$INDENT /" $HEADERS
+        TL="$TL#"
+        INDENT="    $INDENT"
+        let SIZE=SIZE+1
+    done 
 }
 
 
@@ -45,7 +52,7 @@ function makeToc {
     # Pull out all the header tags
     # then trim the first two lines (i.e. repo name & `## Contents` itself)
     # then write to filename passed as $OUTPUT
-    grep -E "^\#+ [A-Za-z]+" $INPUT|tail -n+$SKIP > $OUTPUT
+    grep -E "^\#+ .+" $INPUT|tail -n+$SKIP > $OUTPUT
 
     generateTree $OUTPUT
 
@@ -56,7 +63,7 @@ function makeToc {
     sed -i -r "s/\((#[A-Za-z]+) ([A-Za-z]+)\)$/(\1-\2)/" $OUTPUT
 
     # Set all anchor tags to lowercase to link properly
-    sed -i -r 's/(\(#[A-Za-z]+-?[A-Za-z]+\)$)/\L\1/' $OUTPUT
+    sed -i -r 's/(\(#[A-Za-z\-]+\)$)/\L\1/' $OUTPUT
 
     # Log the final result written to $OUTPUT
     cat $OUTPUT
